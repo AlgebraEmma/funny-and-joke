6a08194e1284c0bd6c191c5ff120f54a6ffa69f5
AlgebraEmma <83509406+AlgebraEmma@users.noreply.github.com>
2024-04-16 18:46:54 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-let output = document.getElementById("output");
-output.innerHTML= ">>>แค่กดชีวิตก็เปลี่ยน<<<";
-
-
-function my() {
-  output.innerHTML = "ควย";
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  var output = document.getElementById("output_text");
+  //document.getElementById("demo").innerHTML = x;
+  if(x=="เกม"){
+    output.innerHTML="คนดี";
+  }
+  else if(x=="ภูผา"){
+    output.innerHTML="หลานย่าวาส";
+  }
+  else if(x=="ปรางค์"){
+    output.innerHTML="แม่น้องเวฬา";
+  }
+  else if(x=="เอก"){
+    output.innerHTML="เอกดาวปีศาจ";
+  }
+  else{
+    output.innerHTML="fail";
+  }
 }
-output.addEventListener("click",my)
